22d11e0d5be7f3248e6a56c307bab4a4a6834d8b
zMattew <mat.mat03@hotmail.com>
2025-08-12 19:28:34 +0200

    added adapter to edge auth

--- auth/src/edge.ts
+++ auth/src/edge.ts
@@ -1,7 +1,10 @@
 import NextAuth from "next-auth"
 import authConfig from "./auth.config.ts"
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import client from "@repo/db/client"
 
 export const nextAuth = NextAuth({
-    ...authConfig
+    ...authConfig,
+    adapter: PrismaAdapter(client),
 })
 
