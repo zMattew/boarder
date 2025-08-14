3b9c29d4d95a17e440eb477b552be6ea18d1583a
zMattew <mat.mat03@hotmail.com>
2025-08-14 16:30:17 +0200

    moved migration and schema to db/src folder

--- db/src/prisma.config.ts
+++ db/src/prisma.config.ts
@@ -1,10 +1,9 @@
-import path from "node:path";
 import type { PrismaConfig } from "prisma";
 import "dotenv/config";
 
 export default {
-  schema: path.join("schema.prisma"),
+  schema: "./schema/prisma",
   migrations:{
-    path: path.join("migrations"),
+    path: "./migrations",
   }
 } satisfies PrismaConfig;
