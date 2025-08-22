45d0ca6bf61cfa29aa2f481805ccdabf3b1594c3
zMattew <mat.mat03@hotmail.com>
2025-08-22 16:39:31 +0200

    added queryCompiler to prisma generator

--- db/src/client.ts
+++ db/src/client.ts
@@ -1,9 +1,11 @@
 import { PrismaClient } from "../generated/client.ts";
+import { PrismaPg } from '@prisma/adapter-pg'
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL })
 const client =
-    globalForPrisma.prisma || new PrismaClient();
+    globalForPrisma.prisma || new PrismaClient({ adapter });
 export default client
 export * from "../generated/client.ts"
 
