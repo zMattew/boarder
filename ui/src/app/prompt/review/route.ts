f6703356c242c00f2e78a7d07479446258a68a40
zMattew <mat.mat03@hotmail.com>
2025-08-31 20:16:57 +0200

    added auth check to prompt route

--- ui/src/app/prompt/review/route.ts
+++ ui/src/app/prompt/review/route.ts
@@ -1,14 +1,23 @@
+import { auth } from "@/lib/auth";
+import { actionLimiter } from "@/lib/limiter";
 import { ToolMessage, ComponentRespones, reviewComponent } from "@repo/core/agent";
+import client from "@repo/db/client";
 import { NextRequest } from "next/server";
 
 export async function POST(req: NextRequest) {
+    const session = await auth()
+    if (!session?.user) throw "Unauthorized"
+    const { success } = await actionLimiter.limit(session.user.id ?? "")
+    if (!success) throw "Too many request"
     const data = await req.formData()
     const component = data.get("component") as string
     const llmId = data.get("provider") as string //ollama local
     const model = data.get("model") as string
     const prompt = data.get("prompt") as string
-
     const response = await reviewComponent(llmId, model, prompt, component)
+    const projectId = req.cookies.get("selected-project")?.value
+    const project = await client.project.findUnique({ where: { id: projectId, OR: [{ team: { some: { userId: session.user.id } } }, { ownerId: session.user.id }], llms: { some: { id: llmId } },view:{some:{components:{some:{id:component}}}} } })
+    if (!project) throw "Unauthorized"
     return new Response(new ReadableStream({
         async pull(controller) {
             try {
@@ -25,7 +34,7 @@ export async function POST(req: NextRequest) {
                     }
                     if (value?.generate_structured_response) {
                         const component = (value?.generate_structured_response as { structuredResponse: ComponentRespones }).structuredResponse
-                        controller.enqueue(JSON.stringify({ component ,threadId:response.threadId,sourceId:component }))
+                        controller.enqueue(JSON.stringify({ component, threadId: response.threadId, sourceId: component }))
                     }
 
                 }
--- ui/src/app/prompt/route.ts
+++ ui/src/app/prompt/route.ts
@@ -1,13 +1,22 @@
+import { auth } from "@/lib/auth";
+import { actionLimiter } from "@/lib/limiter";
 import { promptComponent, ToolMessage, ComponentRespones } from "@repo/core/agent";
+import client from "@repo/db/client";
 import { NextRequest } from "next/server";
 
 export async function POST(req: NextRequest) {
+    const session = await auth()
+    if (!session?.user) throw "Unauthorized"
+    const { success } = await actionLimiter.limit(session.user.id ?? "")
+    if (!success) throw "Too many request"
     const data = await req.formData()
     const source = data.get("source") as string
     const llmId = data.get("provider") as string //ollama local
     const model = data.get("model") as string
     const prompt = data.get("prompt") as string
-
+    const projectId = req.cookies.get("selected-project")?.value
+    const project = await client.project.findUnique({where:{id:projectId,OR:[{team:{some:{userId:session.user.id}}},{ownerId:session.user.id}],llms:{some:{id:llmId}},sources:{some:{id:source}}}})
+    if(!project) throw "Unauthorized"
     const response = await promptComponent(llmId, model, prompt, source)
     return new Response(new ReadableStream({
         async pull(controller) {
@@ -25,7 +34,7 @@ export async function POST(req: NextRequest) {
                     }
                     if (value?.generate_structured_response) {
                         const component = (value?.generate_structured_response as { structuredResponse: ComponentRespones }).structuredResponse
-                        controller.enqueue(JSON.stringify({ component ,threadId:response.threadId,sourceId:source }))
+                        controller.enqueue(JSON.stringify({ component, threadId: response.threadId, sourceId: source }))
                     }
 
                 }
