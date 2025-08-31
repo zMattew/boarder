3281ab88b24668352d516f02f6df9873bd98788b
zMattew <mat.mat03@hotmail.com>
2025-08-31 20:04:27 +0200

    refactored page to use the streaming response to check the progress

--- ui/src/components/form/review-component.tsx
+++ ui/src/components/form/review-component.tsx
@@ -2,14 +2,16 @@
 import { Providers } from "@repo/db/client";
 import { Combobox } from "../combobox";
 import { useComponent } from "../component/context";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useProject } from "@/hooks/project-context";
 import { Textarea } from "../shadcn/textarea";
 import { toast } from "sonner";
-import { review } from "@/lib/component";
+import { review, updateComponent } from "@/lib/component";
 import { Button } from "../shadcn/button";
 import { useFetcher } from "../component/fetch-context";
 import { ModelPicker } from "./model-picker";
+import { ComponentRespones } from "@repo/core/agent";
+import { Loader2 } from "lucide-react";
 
 export function ReviewComponentForm() {
     const { currentProject } = useProject()
@@ -19,9 +21,27 @@ export function ReviewComponentForm() {
     const [model, setModel] = useState<string>();
     const [userPrompt, setPrompt] = useState<string>();
     const [isLoading, setLoading] = useState<boolean>(false);
-
-    return <form action={async () => {
+    const [state, setState] = useState<Record<string, unknown>>({})
+    const [generatedComponent, setComponent] = useState<{ threadId: string, sourceId: string, component: ComponentRespones }>()
+    const [message, setMessage] = useState<string>()
+    useEffect(() => {
+        switch (state.tool) {
+            case "get_components_list":
+                setMessage(state.status == "success" ? "Retrieved available components" : "Failed retrieve components. Retrying")
+                break
+            case "get_database_schema":
+                setMessage(state.status == "success" ? "Retrieved database schema" : "Failed retrieve database schema. Retrying")
+                break
+            case "test_query":
+                setMessage(state.status == "success" ? "Test successful" : "Query malformed. Updating query")
+                break
+        }
+        if (state.component)
+            setComponent(state as { threadId: string, sourceId: string, component: ComponentRespones })
+    }, [state])
+    return <><form action={async () => {
         setLoading(true);
+        setComponent(undefined)
         const formData = new FormData();
         try {
             if (!component.id) throw "No source selected";
@@ -39,11 +59,24 @@ export function ReviewComponentForm() {
             if (!userPrompt) throw "No prompt found";
             formData.append("prompt", userPrompt as string);
 
-            const reviewedComponent = await review(formData);
-            if (reviewedComponent) refetch()
-            toast.success(
-                `Component updated`,
-            );
+            setMessage("Sending request")
+            const res = await fetch(`/prompt/review`, { method: "POST", body: formData })
+            if (!res.ok || !res.body) throw new Error()
+            const reader = res.body.getReader()
+            const decoder = new TextDecoder()
+            setMessage("Request accepted")
+            while (true) {
+                const { done, value } = await reader.read();
+                if (done) {
+                    setMessage(undefined)
+                    return;
+                } else {
+                    const update = decoder.decode(value)
+                    const updateObj = JSON.parse(update)
+                    console.log(updateObj)
+                    setState(updateObj)
+                }
+            }
         } catch (error) {
             toast.error(
                 `Failed to create component: ${error}`,
@@ -70,16 +103,44 @@ export function ReviewComponentForm() {
         {provider &&
             <ModelPicker provider={provider} setModel={setModel} />
         }
-        <Textarea
-            onChange={(e) => setPrompt(e.target.value)}
-            name="prompt"
-            placeholder="prompt"
-            required
-        />
+        <div className="flex flex-col gap-1 justify-start">
+            <Textarea
+                onChange={(e) => setPrompt(e.target.value)}
+                name="prompt"
+                placeholder="prompt"
+                required
+            />
+            <span className={`${isLoading && !generatedComponent  ? '' : 'opacity-0'} flex flex-row gap-1 items-center text-xs text-muted-foreground`}><Loader2 className="animate-spin" size={10} />{message}</span>
+        </div>
         <Button
             type="submit"
             disabled={isLoading}>
             Edit
         </Button>
-    </form>;
+    </form>
+        {
+            generatedComponent ?
+                <>
+                    Type : {generatedComponent?.component.name}<br />
+                    Description: {generatedComponent?.component.description}<br />
+                    <Button
+                        disabled={isLoading}
+                        onClick={async () => {
+                            setLoading(true)
+                            try {
+                                if (!generatedComponent?.component.name || !generatedComponent?.component?.query || !generatedComponent?.sourceId || !generatedComponent.threadId) throw "Detected a partial component, not saved"
+                                const res = await updateComponent(component.id, generatedComponent.component.name, generatedComponent.component.query, generatedComponent.component.description, generatedComponent.component.keys)
+                                if (!res) throw "Component updated"
+                                await refetch()
+                            } catch (error) {
+                                toast.error(`${error}`)
+                            } finally {
+                                setLoading(false)
+                            }
+                        }}>
+                        Save
+                    </Button>
+                </>
+                : undefined
+        }</>
 }
