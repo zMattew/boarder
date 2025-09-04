a22d6d25ea01059a53454a51690f4c7f3e71ad45
zMattew <mat.mat03@hotmail.com>
2025-09-04 18:20:45 +0200

    get currentProject from getMemberRole

--- ui/src/lib/role.ts
+++ ui/src/lib/role.ts
@@ -6,7 +6,7 @@ export async function getMemberRole() {
     const session = await auth();
     if (!session?.user?.id) throw "Unauthorized";
     const projectId = (await cookies()).get("selected-project")?.value as string;
-    return {userId:session.user.id,role:await getMemberRoleFromCookie(projectId, session.user.id)};
+    return {userId:session.user.id,role:await getMemberRoleFromCookie(projectId, session.user.id),selectedProject:projectId};
 }
 
 
